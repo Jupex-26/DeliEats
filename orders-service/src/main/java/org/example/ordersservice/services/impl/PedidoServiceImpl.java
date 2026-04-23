c1e204779b82ee9069611b56f66d8adee23ef5bc
JuanPedro <juan.exposito@nter.es>
2026-04-23 17:16:55 +0200

    feat(crud-pedido): Completados los métodos de PedidoService agregando comportamientos en la entidad como calcular el total

--- orders-service/src/main/java/org/example/ordersservice/services/impl/PedidoServiceImpl.java
+++ orders-service/src/main/java/org/example/ordersservice/services/impl/PedidoServiceImpl.java
@@ -1,60 +1,76 @@
 package org.example.ordersservice.services.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.example.ordersservice.models.Estado;
 import org.example.ordersservice.models.Pedido;
 import org.example.ordersservice.repositories.PedidoRepository;
+import org.example.ordersservice.services.EstadoService;
 import org.example.ordersservice.services.PedidoService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PedidoServiceImpl implements PedidoService {
     private final PedidoRepository pedidoRepository;
+    private final EstadoService estadoService;
 
     @Override
     public Pedido save(Pedido pedido) {
-        return null;
+        return pedidoRepository.save(pedido);
     }
 
     @Override
     public List<Pedido> findAll() {
-        return List.of();
+        return pedidoRepository.findAll();
     }
 
     @Override
     public Pedido findById(Long id) {
-        return null;
+        return pedidoRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Pedido no encontrado con ID: "+ id));
     }
 
     @Override
     public List<Pedido> findByClienteId(Long clienteId) {
-        return List.of();
+        return pedidoRepository.findAllByClienteId(clienteId);
     }
 
     @Override
     public List<Pedido> findByEstadoId(Long estadoId) {
-        return List.of();
+        return pedidoRepository.findAllByEstadoId(estadoId);
     }
 
     @Override
     public Pedido update(Long id, Pedido pedido) {
-        return null;
+        Pedido exists = findById(id);
+        pedido.setId(exists.getId());
+
+        return pedidoRepository.save(pedido);
     }
 
     @Override
     public Pedido updateEstado(Long id, Long estadoId) {
-        return null;
+        Pedido pedido = findById(id);
+        Estado estado = estadoService.findById(estadoId);
+
+        pedido.cambiarEstado(estado);
+
+        return pedidoRepository.save(pedido);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        pedidoRepository.deleteById(id);
     }
 
     @Override
-    public Double calculateTotal(Long id) {
-        return 0.0;
+    public BigDecimal calculateTotal(Long id) {
+        Pedido pedido = findById(id);
+
+        return pedido.calcularTotal();
     }
 }
