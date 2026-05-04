b8298b4c6141229280088f99859a5bda15c20838
Jupex-26 <juanpe.jpep@gmail.com>
2026-05-04 19:20:38 +0200

    feat(aside): creado el componente aside para un administrador

---
+++ front/src/app/layouts/aside/aside.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, inject, Input, model, OnInit, Output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth/auth-service';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.scss'],
+  standalone: true,
+  imports: [IonicModule, RouterModule],
+})
+export class AsideComponent implements OnInit{
+  isCollapsed = false; // Por defecto mostrado
+
+  ngOnInit() {
+    if (window.innerWidth <= 768) {
+      this.isCollapsed = true;
+    }
+  }
+  toggleAside() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
