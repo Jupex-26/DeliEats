0118662e6c6ad11904ad3e2dbc5d9a11eef537e4
Jupex-26 <juanpe.jpep@gmail.com>
2026-05-16 11:48:35 +0200

    update

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
-  storageUrl: 'http://localhost:8080/api/uploads'
+  apiUrl: 'https://delieats-production.up.railway.app:8080/api',
+  storageUrl: 'https://delieats-production.up.railway.app:8080/api/uploads'
 };
