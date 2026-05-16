5cbf7690576d5d32b3fcd243dec112a4c3d48d85
Jupex-26 <juanpe.jpep@gmail.com>
2026-05-16 11:40:56 +0200

    update

---
+++ front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.delieats.app',
+  appName: 'DeliEats',
+  webDir: 'dist/front/browser'
+};
+
+export default config;
